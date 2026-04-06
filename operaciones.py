d6799993178c206378b9fde0c2a113ddf153aaf4
jerry <jerry.cedeno@uip.edu.pa>
2026-04-06 18:29:29 -0500

    se modifica operaciones

--- operaciones.py
+++ operaciones.py
@@ -1,1 +1,16 @@
+# Operaciones.py
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: No se puede dividir entre cero."
+    return a / b
 
